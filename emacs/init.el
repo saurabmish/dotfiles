cce96513b6adccf3c85ee827b51af4c1ab598da5
Saurabh Mishra <saurab.mish@gmail.com>
2020-09-24 15:31:43 -0700

    added MELPA stable repository

--- emacs/init.el
+++ emacs/init.el
@@ -2,6 +2,13 @@
 ;;;;;;;;;;;;; Backup, Auto-save, and Session Management ;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+; Initialization
+
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+
 ; Baseline
 
 (let ((backup-dir "~/.cache/emacs/backups/")
@@ -142,7 +149,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(wombat))
- '(custom-theme-directory "~/.config/emacs/themes"))
+ '(custom-theme-directory "~/.config/emacs/themes")
+ '(package-selected-packages '(go-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
